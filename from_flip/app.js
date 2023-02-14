6e107f9ddeb1703845f9b9053c3a75ac8035ace5
amandadattalo <32142892+amandadattalo@users.noreply.github.com>
2023-02-14 10:59:35 -0800

    fix types in database

--- from_flip/app.js
+++ from_flip/app.js
@@ -4,7 +4,7 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 8031;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 8051;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
 var db = require('./database/db-connector')
